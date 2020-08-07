976442af6d2cffe9a883218dbec0ad211105ace4
kangyerim <yerimkang521@gmail.com>
2020-08-07 09:38:55 +0900

    reupload

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import { OpinionRegister, OpinionList, OpinionDetail } from "./opinion";
 import './css/tailwind.css';
 import './css/_components.css';
 import './css/_palettes.css';
 import './css/main.css';
-import { StockPage } from "./stocks";
+import { StockPage, StockList } from "./stocks";
 import {TotalBudget} from "./portfolio"
 
 const App = () => {
   return <>
           <Switch>
-              <Route path="/stocklist" component={OpinionList}/>
-              <Route path="/opinionRegister" component={OpinionRegister}/>
-              <Route path="/opinionDetail" component={OpinionDetail}/>
+              <Route path="/stocklist" component={StockList}/>
               <Route path="/stockpage" component={StockPage}/>
               <Route path="/portfolio" component={TotalBudget}/>
           </Switch>
--- src/stocks/index.js
+++ src/stocks/index.js
@@ -2,3 +2,4 @@ export { default as StockPage } from "./StockPage";
 export { default as StockDetail} from "./StockDetail";
 export { default as UnderlinedTabs } from  "./UnderlinedTabs";
 export { default as CandleChart } from "./CandleChart";
+export { default as StockList } from "./StockList";
